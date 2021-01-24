6a20cd854d428f3ddd7bf1fb072ddcfa5a4df93d
shalom dawit <shalom.dawit@gmail.com>
2021-01-23 21:46:54 -0500

    App.js setup, added frontend CRUD requests

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Router>
+      <>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </>
+    </Router>
   );
 }
 
-
 export default App;
+
