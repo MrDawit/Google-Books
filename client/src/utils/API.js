2940f19da4c319b2afdc0f58d47b61c89793981c
Shalom Dawit <Shalom.Dawit@gmail.com>
2024-08-19 22:34:27 -0400

    book search now console.logs results

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
 export default {
-  getBooks: function(q) {
-    return axios.get("/api/google", { params: { q: "title:" + q } });
+  getBooks: function(q,res) {
+    //return axios.get("/api/google", { params: { q: "title:" + q } });
+     return axios.get("https://www.googleapis.com/books/v1/volumes", { params: { q: "title:" + q } 
+    }).then( (res)=>{
+      console.log("u got something!!  "+ res.data.items);
+    }
+  );
   },
   getSavedBooks: function() {
     return axios.get("/api/books");
